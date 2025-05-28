1f406c8d69ccc53bfb4efa6e7738a58169f22bb6
eastha0526 <qeqw3435@naver.com>
2025-05-28 12:25:29 +0900

    add sample args

--- sample.py
+++ sample.py
@@ -5,5 +5,12 @@ results = pipeline.run_comparison(
     lora_file="your_lora_file.safetensors",
     prompt="your_prompt",
     tokens_to_analyze=["token1", "token2", "token3"],
-    output_dir="output"
+    output_dir="output",
+    negative_prompt="your_negative_prompt", # if negative_prompt is None: default negative prompt is used
+    lora_scale=1.0,
+    seed=0,
+    steps=50,
+    guidance_scale=7.5,
+    height=1024,
+    width=1024,
 )
