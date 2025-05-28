9d685c429c961eaa4b0fc1020cfb1347c0b0112c
eastha0526 <qeqw3435@naver.com>
2025-05-28 12:23:27 +0900

    fix

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 from lora_attention_analyzer import LoRAAttentionPipeline
 
-pipeline = LoRAAttentionPipeline("your_model_file.safetensors")
+pipeline = LoRAAttentionPipeline("your_model_file.safetensors", model_type="epsilon") # model_type: epsilon, vpred
 results = pipeline.run_comparison(
     lora_file="your_lora_file.safetensors",
     prompt="your_prompt",
